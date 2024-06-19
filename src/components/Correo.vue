dff4a927fb336623a2c1bc5fc3c2474887e5eced
Angelurr <anvixoli@gmail.com>
2024-06-19 09:28:25 -0500

    nuevo

---
+++ src/components/Correo.vue
@@ -0,0 +1,33 @@
+<script setup>
+function cambiarEstilo(index) {
+    correos.value[index].leido = !correos.value[index].leido;
+}
+
+</script>
+<template>
+    <div class="flex text-black justify-center text-6xl mb-10">
+        Correo
+    </div>
+    
+    <div class="bg-slate-500 w-[50%] ml-28">
+        <section class="max-w-6xl mx-auto grid grid-cols-2 w-[50%]">
+            <div class=" grid grid-cols-1 gap-4 ">
+                <div 
+                    v-for="(correo, index) in correos" 
+                    :key="index" 
+                    @click="cambiarEstilo(index)" 
+                    :class="{
+                        'border-4 border-black': !correo.leido,
+                        'bg-slate-300': correo.leido,
+                        'cursor-pointer': true,
+                        'h-32 w-96': true
+                    }"
+                >
+                    {{ correo.asunto }}
+                    <br>
+                    {{ correo.texto }}
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
--- src/components/Perfil2.vue
+++ src/components/Perfil2.vue
@@ -1,18 +1,18 @@
 
 <script setup>
-import { ref } from 'vue';
+// import { ref } from 'vue';
 
-let contador = ref(29);
+// let contador = ref(29);
 
-let contador1 = ref(3);
-function likes(){
-    contador.value++;
+// let contador1 = ref(3);
+// function likes(){
+//     contador.value++;
 
-}
-function Dislikes(){
-    contador1.value++;
+// }
+// function Dislikes(){
+//     contador1.value++;
 
-}
+// }
 
 
 // let likes = ref(27)
@@ -57,12 +57,83 @@ function Dislikes(){
 //         hasDisliked.value = true;
 //     }
 // }
+// let contador = ref(29);
+// let contador1 = ref(3);
+
+// let hasLiked = ref(false);
+// let hasDisliked = ref(false);
+
+// function likes() {
+//     if (!hasLiked.value && !hasDisliked.value) {
+//         contador.value++;
+//         hasLiked.value = true;
+//     }
+// }
+
+// function dislikes() {
+//     if (!hasLiked.value && !hasDisliked.value) {
+//         contador1.value++;
+//         hasDisliked.value = true;
+//     }
+// }
+
+import { ref } from 'vue';
+
+  const contador = ref(29);
+  const contador1 = ref(3);
+  const haDadoLike = ref(false);
+  const haDadoDislike = ref(false);
+
+  function likes() {
+    if (!haDadoLike.value) {
+      contador.value++;
+      haDadoLike.value = true;
+    }
+  }
+
+  function dislikes() {
+    if (!haDadoDislike.value) {
+      contador1.value++;
+      haDadoDislike.value = true;
+    }
+  }
+
+
+//   const likesCount = ref(0);
+// const dislikesCount = ref(0);
+// const liked = ref(false);
+// const disliked = ref(false);
+
+// function likes() {
+//     if (!liked.value) {
+//         if (disliked.value) {
+//             dislikesCount.value--;
+//             disliked.value = false;
+//         }
+//         likesCount.value++;
+//         liked.value = true;
+//     }
+// }
+
+// function dislikes() {
+//     if (!disliked.value) {
+//         if (liked.value) {
+//             likesCount.value--;
+//             liked.value = false;
+//         }
+//         dislikesCount.value++;
+//         disliked.value = true;
+//     }
+// }
 
 
 
 
 
 
+
+</script>
+<script setup>
 </script>
 
 
@@ -124,7 +195,7 @@ function Dislikes(){
                 <svg @click="likes()" class="w-6 h-6 " xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3"/></svg>
                 
                 <p>{{ contador }}</p>
-                <svg @click="Dislikes()" class="ml-6 w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M7.498 15.25H4.372c-1.026 0-1.945-.694-2.054-1.715a12 12 0 0 1-.068-1.285c0-2.848.992-5.464 2.649-7.521C5.287 4.247 5.886 4 6.504 4h4.016a4.5 4.5 0 0 1 1.423.23l3.114 1.04a4.5 4.5 0 0 0 1.423.23h1.294M7.499 15.25c.618 0 .991.724.725 1.282A7.5 7.5 0 0 0 7.5 19.75A2.25 2.25 0 0 0 9.75 22a.75.75 0 0 0 .75-.75v-.633c0-.573.11-1.14.322-1.672c.304-.76.93-1.33 1.653-1.715a9 9 0 0 0 2.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384m-10.253 1.5H9.7m8.075-9.75q.015.075.05.148a8.95 8.95 0 0 1 .925 3.977a8.95 8.95 0 0 1-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368c.339 1.11.521 2.287.521 3.507c0 1.553-.295 3.036-.831 4.398c-.306.774-1.086 1.227-1.918 1.227h-1.053c-.472 0-.745-.556-.5-.96a9 9 0 0 0 .303-.54"/></svg>
+                <svg @click="dislikes()" class="ml-6 w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M7.498 15.25H4.372c-1.026 0-1.945-.694-2.054-1.715a12 12 0 0 1-.068-1.285c0-2.848.992-5.464 2.649-7.521C5.287 4.247 5.886 4 6.504 4h4.016a4.5 4.5 0 0 1 1.423.23l3.114 1.04a4.5 4.5 0 0 0 1.423.23h1.294M7.499 15.25c.618 0 .991.724.725 1.282A7.5 7.5 0 0 0 7.5 19.75A2.25 2.25 0 0 0 9.75 22a.75.75 0 0 0 .75-.75v-.633c0-.573.11-1.14.322-1.672c.304-.76.93-1.33 1.653-1.715a9 9 0 0 0 2.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384m-10.253 1.5H9.7m8.075-9.75q.015.075.05.148a8.95 8.95 0 0 1 .925 3.977a8.95 8.95 0 0 1-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368c.339 1.11.521 2.287.521 3.507c0 1.553-.295 3.036-.831 4.398c-.306.774-1.086 1.227-1.918 1.227h-1.053c-.472 0-.745-.556-.5-.96a9 9 0 0 0 .303-.54"/></svg>
                 <p>{{ contador1 }}</p>
             </div>
         </div>
